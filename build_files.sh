41fcb55601c408a6f7e9e3df85e867416e66f696
Abrar <mdabrar.dev@gmail.com>
2025-03-11 16:30:06 +0530

    Make build script executable

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,3 @@
 apt-get update && apt-get install -y python3.11 python3-pip
-pip install -r requirements.txt
-python3.11 manage.py collectstatic
+pip3 install -r requirements.txt
+python3 manage.py collectstatic --noinput
