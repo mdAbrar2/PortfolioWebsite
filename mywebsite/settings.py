51c3db8e6f34e55e8915f6c16d4b9c90958a1e0a
Abrar <mdabrar.dev@gmail.com>
2025-03-11 21:41:40 +0530

    trying to debug error and deploy the project

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -102,12 +102,20 @@ WSGI_APPLICATION = 'mywebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.dummy'
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
