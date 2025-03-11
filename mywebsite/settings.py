a82255c0dc57ab41976e312a29b4f6ae7113cc37
Abrar <mdabrar.dev@gmail.com>
2025-03-11 17:00:24 +0530

    deploying

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -152,7 +152,8 @@ USE_TZ = True
 # Static files settings
 STATIC_URL = "/static/"
 STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build')
 
 
 # MEDIA_URL = "images/"
